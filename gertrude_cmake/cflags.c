bb770f08941fcc7c80cc3c881b05eed11e23e7df
Ash GAUDENCIO <guillaume.gaudencio@epitech.eu>
2024-02-09 14:30:33 +0100

    [ADD] File Structure for generator
    Will switch g_cmake to an addon

---
+++ gertrude_cmake/cflags.c
@@ -0,0 +1,20 @@
+/*
+** GERTRUDE PROJECT, 2024
+** gertrude-cli
+** File description:
+** cflags.c
+*/
+
+#include "../include/g_cmake.h"
+#include "../include/text_mod.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+
+void set_cflags(g_cmake_t *project)
+{
+    char *buf = malloc(32000);
+
+    printf("%sWORK IN PROGRESS DNF%s\n", YELLOW, NC);
+    // printf("%sSetting compilation flags (default: -g3)%s: ", GREEN, NC);
+}
---
+++ gertrude_cmake/gen_cmake.c
@@ -0,0 +1,14 @@
+/*
+** GERTRUDE PROJECT, 2024
+** gertrude-cli
+** File description:
+** gen_cmake.c
+*/
+
+
+#include "../include/g_cmake.h"
+
+int generate_list(g_cmake_t *project)
+{
+    return 0;
+}
